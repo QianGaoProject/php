3796881f1b4a0233a7de0c3c8881ef8b04d275dd
zdf <dfff>
2018-04-23 17:45:34 -0400

    created netbeans structure. modified database schema. drafted business logic

---
+++ easymove1_netbeans_structure/includes/classes/Quote.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Quote
+ *
+ * @author dongf
+ */
+class Quote {
+    //put your code here
+}
---
+++ easymove1_netbeans_structure/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--Custom styles-->
+    <link rel="stylesheet" type="text/css" href="css/easymove-md.css" />
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse " style="min-height:130px; height:130px; background-color:#326898"  >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a style="padding: 0 10px 0 0; min-height:130px" class="navbar-brand" href="#" ><img id="nav_img" src="images/logo.jpg" alt="Easy Logo" height="130" /></a>
+            </div>
+            <div style="margin-top:8px ; text-align:left; font-family:'Segoe Print'; font-size:medium; font-weight:700; color:#fff">
+                Pour un déménagement tranquille, <br/>efficace et pas cher!
+            </div>
+            <br />
+            <div class="collapse navbar-collapse">
+
+                <ul class="nav navbar-nav"> 
+                    <li class="active"><a href="#">Accueil</a></li>
+                    <li><a href="#moving">Moving</a></li>
+                    <li><a href="#rates">Rates</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+            
+            
+        </div>
+    </nav>
+    <hr style=""/>
+    <div class="jumbotron">
+        <div class="container">
+
+            <div >
+                <h1>Bootstrap starter template</h1>
+                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+            </div>
+
+        </div><!-- /.container -->
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
